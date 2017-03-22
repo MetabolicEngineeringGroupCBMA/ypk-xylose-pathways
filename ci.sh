f003c610bd440a915dffb5dce43295baf95c0e44
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-03-22 11:49:10 +0000

    added travis

--- ci.sh
+++ ci.sh
@@ -3,14 +3,10 @@ wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O
 bash Miniconda_latest.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 rm Miniconda_latest.sh
-conda config --set binstar_upload no --set always_yes yes --set show_channel_urls yes
+conda config --set always_yes yes --set show_channel_urls yes
 conda update -qy conda
 conda config --add channels BjornFJohansson
-conda create -qy -n testenv python=3.5 pydna nbval pytest lxml requests
+conda create -qy -n testenv python=3.5 pydna=1.2.0 nbval pytest lxml requests
 source activate testenv
-which python
-python --version
-conda install pydna
-
 
 python run_test.py
