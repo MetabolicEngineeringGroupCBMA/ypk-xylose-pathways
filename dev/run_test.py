1afdcea8ac012a2e67671481a4426c423ae26704
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-04-04 15:34:15 +0100

    added return code

--- dev/run_test.py
+++ dev/run_test.py
@@ -3,9 +3,6 @@
 import pytest
 import os
 import pathlib
-import sys
-
-exit_=0
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -13,12 +10,12 @@ for dirpath, dirnames, filenames in os.walk(thisdir):
     if dirpath.startswith(('.','_')): continue
     for file_ in filenames:
         if not file_.endswith(".ipynb"): continue
-        if file_=="index.ipynb": continue
         p = pathlib.Path(os.path.join(dirpath, file_))
         if any( [f for f in p.parts if f.startswith(("_","."))] ): continue    
         os.chdir(dirpath)
-        print(file_)
-        exit_+=pytest.cmdline.main( ["--verbose", "--capture=no", "--nbval", file_] )
-print("run_test.py finished with exit code {}".format(exit_))
+        pytest.cmdline.main( [file_,"--verbose", "--capture=no", "--nbval"] )
+print("run_test.py finished.")
 
-sys.exit(exit_)
+
+
+
