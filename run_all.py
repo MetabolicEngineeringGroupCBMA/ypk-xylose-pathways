3581b0c5bddcbf152a8faac047a0192ed8b29d42
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-04-04 15:20:18 +0100

    run all

--- run_all.py
+++ run_all.py
@@ -9,7 +9,7 @@ thisdir = os.path.dirname(os.path.realpath(__file__))
 for dirpath, dirnames, filenames in os.walk(thisdir):
     if dirpath.startswith(('.','_')): continue
     for file_ in filenames:
-        if not file_=="pYPK5.ipynb": continue #################################
+        #if not file_=="pYPK5.ipynb": continue #################################
         if not file_.endswith(".ipynb"): continue
         p = pathlib.Path(os.path.join(dirpath, file_))
         if any( [f for f in p.parts if f.startswith(("_","."))] ): continue    
