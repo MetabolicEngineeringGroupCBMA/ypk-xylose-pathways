ad73cd35118dc9912fa4a191ee51ad621b1ee05a
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-11-02 09:03:14 +0000

    removed most mumberg vectors

--- run_all_notebooks.sh
+++ run_all_notebooks.sh
@@ -4,6 +4,6 @@ shopt -s globstar
 cd notebooks
 
 # command to run all notebooks
-jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors **/*.ipynb
-
+#jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors **/*.ipynb
+jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace                **/*.ipynb
 exit $?
